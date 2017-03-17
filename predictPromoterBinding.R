572ae1c27c1c759495ff825d16a8f1c61fc85aa7
Felix Struebing <fstrueb@emory.edu>
2017-03-17 13:38:31 -0400

    reformatted code, added magritrrd dependency for pipes

--- predictPromoterBinding.R
+++ predictPromoterBinding.R
@@ -1,13 +1,18 @@
 # function to scan promoters defined by CAGE expression of dataset of interest for TFBS
 
-predictPromoterBinding = function(query, motif.list, input.assembly, p.val,...) {
+predictPromoterBinding = function(query,
+                                  motif.list,
+                                  input.assembly,
+                                  p.val,
+                                  ...) {
   options(stringsAsFactors = FALSE)
+  library(magrittr)
   
   anno = vector()
   if (input.assembly == 'mm10') {
     anno = BSgenome.Mmusculus.UCSC.mm10::Mmusculus
   }
-  if(input.assembly == 'mm9') {
+  if (input.assembly == 'mm9') {
     anno = BSgenome.Mmusculus.UCSC.mm9::Mmusculus
   }
   
@@ -21,7 +26,7 @@ predictPromoterBinding = function(query, motif.list, input.assembly, p.val,...) 
   
   # motif.list can either be a vector with JASPAR IDs or character string 'all'
   opts = list()
-  if(motif.list == 'all') {
+  if (motif.list == 'all') {
     opts['all'] = T
   } else {
     opts[['ID']] = motif.list
@@ -30,7 +35,12 @@ predictPromoterBinding = function(query, motif.list, input.assembly, p.val,...) 
   
   # scan the sequences!
   print('scanning the sequence...')
-  scannedPromoters = TFBSTools::searchSeq(x = motif, subject = dna, min.score = '80%', mc.cores = 8L)
+  scannedPromoters = TFBSTools::searchSeq(
+    x = motif,
+    subject = dna,
+    min.score = '80%',
+    mc.cores = 8L
+  )
   print('gathering results...')
   result = TFBSTools::writeGFF3(scannedPromoters, scoreType = 'absolute')
   
@@ -43,11 +53,23 @@ predictPromoterBinding = function(query, motif.list, input.assembly, p.val,...) 
   
   # return the combined dataframe
   stopifnot(length(dna) == length(query))
-
-  result = with(result, cbind(result, reshape::colsplit(attributes, split = "\\;", names = c('TF', 'TF_class', 'TF_sequence')))) %>%
+  
+  result = with(result, cbind(
+    result,
+    reshape::colsplit(
+      attributes,
+      split = '\\;',
+      names = c('TF', 'TF_class', 'TF_sequence')
+    )
+  )) %>%
     dplyr::mutate(seqname = as.numeric(seqname)) %>%
     dplyr::select(-source, -feature, -frame, -attributes) %>%
-    dplyr::rename(motif_start = start, motif_end = end, motif_strand = strand, motif_score = score)
+    dplyr::rename(
+      motif_start = start,
+      motif_end = end,
+      motif_strand = strand,
+      motif_score = score
+    )
   # caveat --> naming convention of column 'geneSymbol'
   helper = query[result$seqname]@elementMetadata$geneSymbol
   seqnames = query[result$seqname]@seqnames
@@ -55,7 +77,7 @@ predictPromoterBinding = function(query, motif.list, input.assembly, p.val,...) 
   names(seqranges) = c('query_start', 'query_end', 'query_width')
   strand = query[result$seqname]@strand
   result2 = cbind(helper, seqnames, seqranges, strand, result)
-
+  
   if (p.val == T) {
     print('calculating p-values...')
     scannedPromoters.pval = TFBSTools::pvalues(scannedPromoters)
@@ -66,11 +88,11 @@ predictPromoterBinding = function(query, motif.list, input.assembly, p.val,...) 
   # cosmetic changes
   result2 = result2 %>%
     dplyr::select(-seqname) %>%
-    dplyr::rename(promoter = helper, query_strand = strand, query_chromosome = seqnames)
+    dplyr::rename(
+      promoter = helper,
+      query_strand = strand,
+      query_chromosome = seqnames
+    )
   
   return(result2)
 }
-
-
-
-
