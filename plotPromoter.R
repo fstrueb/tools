ceb3bb7d0059fae1e19f884f638688367c3d6e51
Felix Struebing <fstrueb@emory.edu>
2017-03-30 16:49:26 -0400

    added motif_selection, smoothed linear heatmap for TFBS hits, fixed geneRegion plot, fixed dependencies, added dnase track for retina

--- plotPromoter.R
+++ plotPromoter.R
@@ -1,16 +1,6 @@
 # function to plot and visualize a promoter track
 
-## xample
-# test.gr = sox11InBdnf %>%
-#   mutate(start = query_start+motif_start, end = query_start+motif_end, strand = motif_strand) %>%
-#   makeGRangesFromDataFrame(., keep.extra.columns = T, seqnames.field = 'query_chromosome')
-# chr = 'chr2'
-
-# range = bdnf %>%
-#   mutate(start = query_start+motif_start, end = query_start+motif_end, strand = motif_strand)
-
-
-plotPromoter = function(range, chr, from = NULL, to = NULL) {
+plotPromoter = function(range, chr, motif.selection, from = NULL, to = NULL) {
   ## check input
   # if (class(range) != 'GRanges') {
   #   stop('You must supply a GenomicRanges object for range')
@@ -19,8 +9,7 @@ plotPromoter = function(range, chr, from = NULL, to = NULL) {
   ## first remove unwanted scaffolds and convert seqnames to UCSC style
   #range = standardizeSeqlevels(range, seqlevels = 'mouse', type = 'UCSC')
   
-  obj = as.data.frame(range) %>%
-    dplyr::mutate(midpoint = (end - start) / 2)
+  
   
   if (is.null(chr)) {
     stop('You must supply a chromosome')
@@ -30,54 +19,65 @@ plotPromoter = function(range, chr, from = NULL, to = NULL) {
     chr = paste0('chr', chr)
   }
   
-  if (is.null(from) | is.null(to)) {
-    start = min(obj$start)
-    end = max(obj$end)
-  }
+  
   
   # note: single hits for motif_ID are not supported
   
-  bw = 'SJ' 
-  n = 512
-  adjust = 0.1
+  # bw = 'SJ' 
+  # n = 512
+  # adjust = 0.1
   
-  m = c('MA0596.1', 'MA0075.1')
-  
-  range = range %>%
+  obj = as.data.frame(range) %>%
+    dplyr::mutate(plot_start = query_start+motif_start) %>%
+    dplyr::mutate(plot_end = query_start+motif_end) %>%
     dplyr::filter(motif_ID %in% m)
   
-  nonSingles = range %>%
+  nonSingles = obj %>%
     dplyr::group_by(motif_ID) %>%
     dplyr::tally() %>%
     dplyr::filter(n > 1)
   
-  ## constructed with smoothed linear heatmap
-  # tfbsCoverage = range %>% 
-  #   dplyr::filter(motif_ID %in% nonSingles$motif_ID) %>%
-  #   ggplot(aes(x = start, y = factor(motif_ID)))+
-  #   stat_density(aes(fill = ..density..), geom = "raster", position = "identity") +
-  #   facet_wrap(~motif_strand) +
-  #   scale_fill_continuous(guide = F) +
-  #   theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
-  #   xlim(start, end) +
-  #   labs(y = '')
+  m = motif.selection
   
-  ## constructed with ggbio
-  ## not pretty
+  if (is.null(from) | is.null(to)) {
+    start = min(obj$query_start)
+    end = max(obj$query_end)
+  }
+  
+  # constructed with ggplot smoothed linear heatmap
+tfbsCoverage = obj %>%
+    dplyr::filter(motif_ID %in% nonSingles$motif_ID) %>%
+    dplyr::filter(motif_ID %in% m) %>%
+    ggplot2::ggplot(ggplot2::aes(x = plot_start, y = as.factor(motif_ID))) +
+    ggplot2::stat_density(ggplot2::aes(fill = ..density..), geom = "raster", position = "identity", adjust = 0.05) +
+    #stat_bin2d(aes(fill = ..count..), binwidth = c(3,1)) +
+    #geom_point(stat = 'identity', size = 10) +
+    ggplot2::geom_rug(sides = 't') +
+    #facet_wrap(~motif_strand, nrow = 2) +
+    ggplot2::scale_fill_continuous(guide = F) +
+    ggplot2::theme(axis.text.y = ggplot2::element_blank(), axis.ticks.y = ggplot2::element_blank()) +
+    ggplot2::xlim(start, end) +
+    ggplot2::labs(y = '', x = '')
+  
+  ## constructed with ggbio, not pretty
   # range %>%
   #   ggplot(aes(xmin = start, xmax = end+50, ymin = motif_hit, ymax = motif_hit + 4, fill = motif_name)) +
   #   geom_rect(stat = 'identity') +
   #   facet_wrap(~motif_strand)
   
+  geneRegion = ggplot2::ggplotGrob(ggbio::autoplot(TxDb.Mmusculus.UCSC.mm10.knownGene::TxDb.Mmusculus.UCSC.mm10.knownGene, which = range(x = GenomicRanges::GRanges(seqnames = chr, IRanges::IRanges(start = start, end = end)))) +
+    ggplot2::xlim(start, end))
   
-  geneRegion = ggbio::autoplot(TxDb.Mmusculus.UCSC.mm10.knownGene, which = range(x = GRanges(seqnames = chr, IRanges(start = start, end = end)))) +
-    xlim(start, end)
-  geneRegion = ggplotGrob(geneRegion)
+  # import retina dnase bigwig
+  dnase = rtracklayer::import.bw(con = '../ext/DNaseSeqRetina_SRX191033.bw', selection = rtracklayer::BigWigSelection(ranges = GenomicRanges::GRanges(seqnames = chr, ranges = IRanges::IRanges(start = start, end = end))))
   
-  #gridExtra::grid.arrange(tfbsCoverage, geneRegion, nrow = 2)
+  dnasePlot = ggplot2::ggplotGrob(ggplot2::ggplot(dnase, ggplot2::aes(x = start, y = score)) +
+    ggplot2::geom_area() +
+    ggplot2::labs(y = '', x = '') +
+    ggplot2::theme(axis.text.y = ggplot2::element_blank(), axis.ticks.y = ggplot2::element_blank()))
   
-  plot(geneRegion)
+  plot = gridExtra::grid.arrange(tfbsCoverage, geneRegion, dnasePlot, nrow = 3)
+  plot(plot)
 
   }
-# 
-# test = plotPromoter(range, 'chr12')
+
