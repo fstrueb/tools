04a582088465f0df93cd8ae28507febf6763eea6
Felix Struebing <fstrueb@emory.edu>
2017-04-08 18:25:29 -0400

    fixed motif and version concatenation error

--- R_functions/siteSetListSummary.R
+++ R_functions/siteSetListSummary.R
@@ -1,6 +1,7 @@
 library(RSQLite)
+library(TFBSTools)
 
-siteSetListSummary = function(query, siteSetList, ) {
+siteSetListSummary = function(query, siteSetList) {
   x = siteSetList
   # x = scannedRange
   
@@ -16,10 +17,11 @@ siteSetListSummary = function(query, siteSetList, ) {
   motif_ID = unlist(lapply(x, function(z) {ID(z@pattern)}))
   motif_lengths = unlist(lapply(x, function(z) {length(z@views)}))
   motifSummary = as.data.frame(t(rbind(motif_ID, motif_lengths)), row.names = F) %>%
-    # dplyr::mutate(motif_ID = stringr::str_sub(motif_IDs, start = 1L, end = 6L)) %>%
-    dplyr::mutate(motif_lengths = as.numeric(motif_lengths)) %>%
-    group_by(motif_ID) %>%
-    summarise(n = sum(motif_lengths))
+    dplyr::mutate(motif_ID.version = motif_ID) %>%
+    dplyr::mutate(motif_version = stringr::str_sub(motif_ID, start = 8L, end = 9L)) %>%
+    dplyr::mutate(motif_ID = stringr::str_sub(motif_ID, start = 1L, end = 6L)) %>%
+    dplyr::mutate(motif_lengths = as.numeric(motif_lengths))
+  
   
   # get results straight from JASPAR SQLite db
   drv = RSQLite::SQLite()
@@ -27,9 +29,16 @@ siteSetListSummary = function(query, siteSetList, ) {
   res = gsub(capture.output(cat(motifSummary$motif_ID, sep = ',')), pattern = ',', replacement = '","')
   resu = dbGetQuery(con, paste0('SELECT * FROM MATRIX WHERE BASE_ID IN ("', res, '")')) %>%
     dplyr::select(-ID, -COLLECTION) %>%
-    tidyr::unite(col = MOTIF_ID, BASE_ID, VERSION, sep = '.')
+    tidyr::unite(col = MOTIF_ID, BASE_ID, VERSION, sep = '.') %>%
+    # dplyr::rename(MOTIF_ID = BASE_ID) %>%
+    dplyr::filter(MOTIF_ID %in% motifSummary$motif_ID.version)
+  
+  motifSummary = motifSummary %>%
+    group_by(motif_ID.version) %>%
+    summarise(n = sum(motif_lengths))
   
   result = resu %>%
-    inner_join(., motifSummary, by = c('MOTIF_ID' = 'motif_ID'))
+    inner_join(., motifSummary, by = c('MOTIF_ID' = 'motif_ID.version')) %>%
+    arrange(-n)
   result
 }
